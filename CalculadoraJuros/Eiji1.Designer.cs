6990b60829597067df3fb300758dc2afd2681f44
Bruno Eiji <brunomine151@outlook.com>
2021-05-26 20:06:54 -0300

    Novo form adicionado !

--- CalculadoraJuros/Eiji1.Designer.cs
+++ CalculadoraJuros/Eiji1.Designer.cs
@@ -104,7 +104,7 @@ namespace CalculadoraJuros
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Sitka Heading", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(267, 25);
+            this.label1.Location = new System.Drawing.Point(267, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(284, 39);
             this.label1.TabIndex = 4;
---
+++ CalculadoraJuros/Eiji3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraJuros
+{
+    public partial class Eiji3 : Form
+    {
+        public Eiji3()
+        {
+            InitializeComponent();
+        }
+    }
+}
